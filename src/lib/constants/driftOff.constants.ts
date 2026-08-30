69887e923724bdb8c455e0167abddce2e4761584
kokkondaBhanuteja <bhanutejakokkonda@gmail.com>
2026-08-30 21:42:26 +0530

    feat(deep-rest): replace YouTube embed with Drive audio link
    Swaps the react-player YouTube video on the Deep Rest landing hero
    for a static thumbnail that links out to a Google Drive-hosted audio
    file, opened in a new tab.

--- src/lib/constants/driftOff.constants.ts
+++ src/lib/constants/driftOff.constants.ts
@@ -3,7 +3,9 @@ import { NTHERAPY_YOUTUBE_VIDEOS } from './enums';
 export const DRIFT_OFF_SESSION_PRICE = 999;
 export const DRIFT_OFF_SESSION_IMAGE = '/drift-off-session.png';
 
-export const DRIFT_OFF_LANDING_VIDEO_URL = NTHERAPY_YOUTUBE_VIDEOS.LANDING_HERO;
+export const DRIFT_OFF_LANDING_AUDIO_URL =
+  'https://drive.google.com/file/d/1FBJ27CNSnMrJ_g7uRu3Q4Hm69OcBMT_d/view?usp=drive_link';
+export const DRIFT_OFF_LANDING_THUMBNAIL = '/deep-rest-thumbnail.png';
 export const DRIFT_OFF_HERO_VIDEO_URL = NTHERAPY_YOUTUBE_VIDEOS.HERO;
 
 export const DRIFT_OFF_HERO_VIDEO_TITLE = 'Sample Guided Meditation';
